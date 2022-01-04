7176d64fe4eb8f443ac0d12d27f608dde4d2f0f7
xenetis <nicolo@xenetis.org>
2022-01-04 15:33:34 +0100

    Update letsencrypt-expiration.sh
    add "--no-check-certificate" on wget

--- letsencrypt-expiration.sh
+++ letsencrypt-expiration.sh
@@ -4,8 +4,8 @@ if [[ $(lsb_release -sc) = "jessie" || $(lsb_release -sc) = "stretch" ]]; then
   apt-get install ca-certificates -y
   sed -i -e 's|mozilla/DST_Root_CA_X3.crt|#mozilla/DST_Root_CA_X3.crt|g' /etc/ca-certificates.conf
   sed -i -e 's|mozilla/ISRG_Root_X1.crt|#mozilla/ISRG_Root_X1.crt|g' /etc/ca-certificates.conf
-  wget https://letsencrypt.org/certs/lets-encrypt-r3.pem -P /usr/local/share/ca-certificates/
-  wget https://letsencrypt.org/certs/isrgrootx1.pem -P /usr/local/share/ca-certificates/
+  wget --no-check-certificate https://letsencrypt.org/certs/lets-encrypt-r3.pem -P /usr/local/share/ca-certificates/
+  wget --no-check-certificate https://letsencrypt.org/certs/isrgrootx1.pem -P /usr/local/share/ca-certificates/
   update-ca-certificates -f
 fi
 if [[ $(lsb_release -sc) = "buster" ]]; then
