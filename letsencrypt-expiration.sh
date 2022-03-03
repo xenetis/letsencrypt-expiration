4d0fcc5644a3a0e19e4da2bdace2e2912ea56b60
p0wer <2510748+p0wertiger@users.noreply.github.com>
2022-03-03 11:44:52 +0100

    Fix file extensions for update-ca-certificates

--- letsencrypt-expiration.sh
+++ letsencrypt-expiration.sh
@@ -6,6 +6,7 @@ if [[ $(lsb_release -sc) = "jessie" || $(lsb_release -sc) = "stretch" ]]; then
   sed -i -e 's|mozilla/ISRG_Root_X1.crt|#mozilla/ISRG_Root_X1.crt|g' /etc/ca-certificates.conf
   wget --no-check-certificate https://letsencrypt.org/certs/lets-encrypt-r3.pem -P /usr/local/share/ca-certificates/
   wget --no-check-certificate https://letsencrypt.org/certs/isrgrootx1.pem -P /usr/local/share/ca-certificates/
+  rename.ul .pem .crt /usr/local/share/ca-certificates/*.pem
   update-ca-certificates -f
 fi
 if [[ $(lsb_release -sc) = "buster" ]]; then
